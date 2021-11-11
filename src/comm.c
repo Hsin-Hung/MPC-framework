def7ef230e58865b1416ea17fa15d77c668c0cad
Christopher Jung <cjung@bu.edu>
2021-11-11 01:55:48 -0500

    replace all MPI dependencies with TCP except MPI_I

--- src/comm.c
+++ src/comm.c
@@ -22,11 +22,8 @@ PRIVATE void check_init(const char *f);
 // initialize MPI, rank, num_parties
 void init(int argc, char** argv)
 {
-  // MPI_Init(&argc, &argv);
   TCP_Init(&argc, &argv);
-  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   TCP_Comm_rank(&rank);
-  // MPI_Comm_size(MPI_COMM_WORLD, &num_parties);
   TCP_Comm_size(&num_parties);
   // this protocol works with 3 parties only
   if (rank == 0 && num_parties != NUM_PARTIES) {
@@ -45,11 +42,9 @@ BShare exchange_shares(BShare s1)
 {
   BShare s2;
   // send s1 to predecessor
-  // MPI_Send(&s1, 1, MPI_LONG_LONG, get_pred(), XCHANGE_MSG_TAG, MPI_COMM_WORLD);
   TCP_Send(&s1, 1, get_pred(), XCHANGE_MSG_TAG);
   // // receive remote seed from successor
-  // MPI_Recv(&s2, 1, MPI_LONG_LONG, get_succ(), XCHANGE_MSG_TAG,
-  //                                           MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
   TCP_Recv(&s2, 1, get_succ(), XCHANGE_MSG_TAG);
   return s2;
 }
@@ -77,13 +72,10 @@ unsigned long long exchange_shares_u(unsigned long long s1)
 {
   unsigned long long s2;
   // send s1 to predecessor
-  // MPI_Send(&s1, 1, MPI_UNSIGNED_LONG_LONG, get_pred(), XCHANGE_MSG_TAG,
-  //                                                      MPI_COMM_WORLD);
+
   TCP_Send(&s1, 1, get_pred(), XCHANGE_MSG_TAG);
   // // receive remote seed from successor
-  // MPI_Recv(&s2, 1, MPI_UNSIGNED_LONG_LONG, get_succ(), XCHANGE_MSG_TAG,
-  //                                                      MPI_COMM_WORLD,
-  //                                                      MPI_STATUS_IGNORE);
+
   TCP_Recv(&s2, 1, get_succ(), XCHANGE_MSG_TAG);
   return s2;
 }
@@ -93,11 +85,9 @@ BitShare exchange_bit_shares(BitShare s1)
 {
   BitShare s2;
   // send s1 to predecessor
-  // MPI_Send(&s1, 1, MPI_C_BOOL, get_pred(), XCHANGE_MSG_TAG, MPI_COMM_WORLD);
   TCP_Send(&s1, 1, get_pred(), XCHANGE_MSG_TAG);
   // // receive remote seed from successor
-  // MPI_Recv(&s2, 1, MPI_C_BOOL, get_succ(), XCHANGE_MSG_TAG, MPI_COMM_WORLD,
-  //                                                         MPI_STATUS_IGNORE);
+  
   TCP_Recv(&s2, 1, get_succ(), XCHANGE_MSG_TAG);
   return s2;
 }
@@ -189,9 +179,11 @@ Data open_b(BShare s)
     Data msg, res = s;
     // P1 receives shares from P2, P3
     // MPI_Recv(&msg, 1, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    // res ^= msg;
+    TCP_Recv(&msg, 1, 1, OPEN_MSG_TAG);
+    res ^= msg;
     // MPI_Recv(&msg, 1, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    // res ^= msg;
+    TCP_Recv(&msg, 1, 2, OPEN_MSG_TAG);
+    res ^= msg;
     
     return res;
   }
@@ -206,23 +198,25 @@ Data open_b(BShare s)
 Data open_bit(BitShare s)
 {
   // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
+  if (rank == 1 || rank == 2) {
   //     MPI_Send(&s, 1, MPI_C_BOOL, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //     return s;
-  // }
-  // else if (rank == 0) {
-  //   bool msg, res = s;
+  TCP_Send(&s, 1, 0, OPEN_MSG_TAG);
+  return s;
+  }
+  else if (rank == 0) {
+  bool msg, res = s;
   //   // P1 receives shares from P2, P3
-  //   MPI_Recv(&msg, 1, MPI_C_BOOL, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   res ^= msg;
-  //   MPI_Recv(&msg, 1, MPI_C_BOOL, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   res ^= msg;
-  //   return res;
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   return 1;
-  // }
+
+    TCP_Recv(&msg, 1, 1, OPEN_MSG_TAG);
+    res ^= msg;
+    TCP_Recv(&msg, 1, 2, OPEN_MSG_TAG);
+    res ^= msg;
+  return res;
+  }
+  else {
+  fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  return 1;
+  }
 }
 
 // [boolean share] open an array of values in P1 (rank=0)
@@ -230,80 +224,87 @@ Data open_bit(BitShare s)
 void open_b_array(BShare *s, int len, Data res[])
 {
   // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(s, len, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  // }
-  // else if (rank == 0) {
-  //   BShare *msg = malloc(len*sizeof(BShare));
-  //   assert(msg!=NULL);
+  if (rank == 1 || rank == 2) {
+    TCP_Send(s, len, 0, OPEN_MSG_TAG);
+  }
+  else if (rank == 0) {
+    BShare *msg = malloc(len*sizeof(BShare));
+    assert(msg!=NULL);
   //   // P1 receives shares from P2, P3
-  //   MPI_Recv(msg, len, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = s[i] ^ msg[i];
-  //   }
+    TCP_Recv(msg, len, 1, OPEN_MSG_TAG);
 
-  //   MPI_Recv(msg, len, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = res[i] ^ msg[i];
-  //   }
-  //   free(msg);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
+    for (int i=0; i<len; i++) {
+      res[i] = s[i] ^ msg[i];
+    }
+
+    TCP_Recv(msg, len, 2, OPEN_MSG_TAG);
+
+    for (int i=0; i<len; i++) {
+      res[i] = res[i] ^ msg[i];
+    }
+    free(msg);
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  }
 }
 
 // [boolean share] open an array of values in P1 (rank=0)
 // MUST BE CALLED ONLY ONCE AS IT MUTATES THE GIVEN TABLE s
 void open_byte_array(char *s, int len, char res[])
 {
-  // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(s, len, MPI_CHAR, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  // }
-  // else if (rank == 0) {
-  //   char *msg = malloc(len*sizeof(char));
-  //   assert(msg!=NULL);
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(msg, len, MPI_CHAR, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = s[i] ^ msg[i];
-  //   }
+  P2, P3 send their shares to P1
+  if (rank == 1 || rank == 2) {
 
-  //   MPI_Recv(msg, len, MPI_CHAR, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = res[i] ^ msg[i];
-  //   }
-  //   free(msg);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
+    TCP_Send(s, len, 0, OPEN_MSG_TAG);
+
+  }
+  else if (rank == 0) {
+    char *msg = malloc(len*sizeof(char));
+    assert(msg!=NULL);
+  // P1 receives shares from P2, P3
+    TCP_Recv(msg, len, 1, OPEN_MSG_TAG);
+
+    for (int i=0; i<len; i++) {
+      res[i] = s[i] ^ msg[i];
+    }
+
+    TCP_Recv(msg, len, 2, OPEN_MSG_TAG);
+
+    for (int i=0; i<len; i++) {
+      res[i] = res[i] ^ msg[i];
+    }
+    free(msg);
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  }
 }
 
 // [arithmetic share] open a value in P1 (rank=0)
 Data open_a(AShare s)
 {
   // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(&s, 1, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //     return s;
-  // }
-  // else if (rank == 0) {
-  //   Data msg, res = s;
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(&msg, 1, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   res += msg;
-  //   MPI_Recv(&msg, 1, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   res += msg;
-  //   return res;
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   return 1;
-  // }
+  if (rank == 1 || rank == 2) {
+  TCP_Send(&s, 1, 0, OPEN_MSG_TAG);
+
+  return s;
+  }
+  else if (rank == 0) {
+    Data msg, res = s;
+    // P1 receives shares from P2, P3
+    TCP_Recv(&msg, 1, 1, OPEN_MSG_TAG);
+
+    res += msg;
+    TCP_Recv(&msg, 1, 2, OPEN_MSG_TAG);
+
+    res += msg;
+    return res;
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+    return 1;
+  }
 }
 
 // [arithmetic share] open an array of arithmetic shares in P1 (rank=0)
@@ -311,28 +312,29 @@ void open_array(AShare *s, int len, Data res[])
 {
 
   // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(s, len, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  // }
-  // else if (rank == 0) {
-  //   AShare *msg = malloc(len*sizeof(AShare));
-  //   assert(msg!=NULL);
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(msg, len, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = s[i] + msg[i];
-  //   }
+  if (rank == 1 || rank == 2) {
+    TCP_Send(s, len, 0, OPEN_MSG_TAG);
+  }
+  else if (rank == 0) {
+    AShare *msg = malloc(len*sizeof(AShare));
+    assert(msg!=NULL);
+    // P1 receives shares from P2, P3
+    TCP_Recv(msg, len, 1, OPEN_MSG_TAG);
 
-  //   MPI_Recv(msg, len, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = res[i] + msg[i];
-  //   }
-  //   free(msg);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
+    for (int i=0; i<len; i++) {
+      res[i] = s[i] + msg[i];
+    }
+
+    TCP_Recv(msg, len, 2, OPEN_MSG_TAG);
+
+    for (int i=0; i<len; i++) {
+      res[i] = res[i] + msg[i];
+    }
+    free(msg);
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  }
 }
 
 // [arithmetic share] open an array of arithmetic shares in P1 (rank=0)
@@ -343,70 +345,71 @@ void open_mixed_array(BShare *s, int rows, int cols, Data res[],
   assert((al + bl) == cols);
   int len = rows * cols;
   // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(s, len, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  // }
-  // else if (rank == 0) {
-  //   BShare *msg = malloc(rows*cols*sizeof(BShare)); // BShare and AShare have equal size
-  //   assert(msg!=NULL);
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(msg, len, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i+=cols) {
-  //       // If aritmetic
-  //       for (int j=0; j<al; j++) {
-  //         res[i+a[j]] = s[i+a[j]] + msg[i+a[j]];
-  //       }
-  //       // If boolean
-  //       for (int j=0; j<bl; j++) {
-  //         res[i+b[j]] = s[i+b[j]] ^ msg[i+b[j]];
-  //       }
-  //   }
+  if (rank == 1 || rank == 2) {
+    TCP_Send(s, len, 0, OPEN_MSG_TAG);
+  }
+  else if (rank == 0) {
+    BShare *msg = malloc(rows*cols*sizeof(BShare)); // BShare and AShare have equal size
+    assert(msg!=NULL);
+    // P1 receives shares from P2, P3
+    TCP_Recv(msg, len, 1, OPEN_MSG_TAG);
 
-  //   MPI_Recv(msg, len, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i+=cols) {
-  //     // If aritmetic
-  //     for (int j=0; j<al; j++) {
-  //       res[i+a[j]] = res[i+a[j]] + msg[i+a[j]];
-  //     }
-  //     // If boolean
-  //     for (int j=0; j<bl; j++) {
-  //       res[i+b[j]] = res[i+b[j]] ^ msg[i+b[j]];
-  //     }
-  //   }
-  //   free(msg);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
+    for (int i=0; i<len; i+=cols) {
+        // If aritmetic
+        for (int j=0; j<al; j++) {
+          res[i+a[j]] = s[i+a[j]] + msg[i+a[j]];
+        }
+        // If boolean
+        for (int j=0; j<bl; j++) {
+          res[i+b[j]] = s[i+b[j]] ^ msg[i+b[j]];
+        }
+    }
+
+    TCP_Recv(msg, len, 2, OPEN_MSG_TAG);
+
+    for (int i=0; i<len; i+=cols) {
+      // If aritmetic
+      for (int j=0; j<al; j++) {
+        res[i+a[j]] = res[i+a[j]] + msg[i+a[j]];
+      }
+      // If boolean
+      for (int j=0; j<bl; j++) {
+        res[i+b[j]] = res[i+b[j]] ^ msg[i+b[j]];
+      }
+    }
+    free(msg);
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  }
 }
 
 void open_bit_array(BitShare *s, int len, bool res[])
 {
-  // P2, P3 send their shares to P1
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(s, len, MPI_C_BOOL, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  // }
-  // else if (rank == 0) {
-  //   BitShare *msg = malloc(len*sizeof(BitShare));
-  //   assert(msg!=NULL);
+  //P2, P3 send their shares to P1
+  if (rank == 1 || rank == 2) {
+      TCP_Send(s, len, 0, OPEN_MSG_TAG);
+  }
+  else if (rank == 0) {
+    BitShare *msg = malloc(len*sizeof(BitShare));
+    assert(msg!=NULL);
 
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(msg, len, MPI_C_BOOL, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = s[i] ^ msg[i];
-  //   }
+    // P1 receives shares from P2, P3
+    TCP_Recv(msg, len, 1, OPEN_MSG_TAG);
+    for (int i=0; i<len; i++) {
+      res[i] = s[i] ^ msg[i];
+    }
 
-  //   MPI_Recv(msg, len, MPI_C_BOOL, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     res[i] = res[i] ^ msg[i];
-  //   }
-  //   free(msg);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
+    TCP_Recv(msg, len, 2, OPEN_MSG_TAG);
+
+    for (int i=0; i<len; i++) {
+      res[i] = res[i] ^ msg[i];
+    }
+    free(msg);
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  }
 }
 
 // [boolean share] open a value in all parties
@@ -415,26 +418,27 @@ Data reveal_b(BShare s)
   Data res = s;
 
   // P2, P3 send their shares to P1 and receive the result
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(&s, 1, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //     MPI_Recv(&res, 1, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  // }
-  // else if (rank == 0) {
-  //   Data msg;
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(&msg, 1, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   res ^= msg;
-  //   MPI_Recv(&msg, 1, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   res ^= msg;
+  if (rank == 1 || rank == 2) {
+    TCP_Send(&s, 1, 0, OPEN_MSG_TAG);
+    TCP_Recv(&res, 1, 0, OPEN_MSG_TAG);
+  }
+  else if (rank == 0) {
+    Data msg;
+    // P1 receives shares from P2, P3
+    TCP_Recv(&msg, 1, 1, OPEN_MSG_TAG);
+    res ^= msg;
+    TCP_Recv(&msg, 1, 2, OPEN_MSG_TAG);
+    res ^= msg;
 
-  //   // P1 sends result to P2, P3
-  //   MPI_Send(&res, 1, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //   MPI_Send(&res, 1, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  //   return 1;
-  // }
+    // P1 sends result to P2, P3
+    TCP_Send(&res, 1, 1, OPEN_MSG_TAG);
+    TCP_Send(&res, 1, 2, OPEN_MSG_TAG);
+
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+    return 1;
+  }
   return res;
 }
 
@@ -442,30 +446,30 @@ Data reveal_b(BShare s)
 void reveal_b_array(BShare *s, int len)
 {
   // P2, P3 send their shares to P1 and receive the result
-  // if (rank == 1 || rank == 2) {
-  //     MPI_Send(s, len, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //     MPI_Recv(s, len, MPI_LONG_LONG, 0, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  // }
-  // else if (rank == 0) {
-  //   Data *msg = malloc(len*sizeof(Data));
-  //   assert(msg!=NULL);
-  //   // P1 receives shares from P2, P3
-  //   MPI_Recv(&msg[0], len, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     s[i]^=msg[i];
-  //   }
-  //   MPI_Recv(&msg[0], len, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   for (int i=0; i<len; i++) {
-  //     s[i]^=msg[i];
-  //   }
-  //   // P1 sends result to P2, P3
-  //   MPI_Send(s, len, MPI_LONG_LONG, 1, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //   MPI_Send(s, len, MPI_LONG_LONG, 2, OPEN_MSG_TAG, MPI_COMM_WORLD);
-  //   free(msg);
-  // }
-  // else {
-  //   fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
-  // }
+  if (rank == 1 || rank == 2) {
+    TCP_Send(s, len, 0, OPEN_MSG_TAG);
+    TCP_Recv(s, len, 0, OPEN_MSG_TAG);
+  }
+  else if (rank == 0) {
+    Data *msg = malloc(len*sizeof(Data));
+    assert(msg!=NULL);
+    // P1 receives shares from P2, P3
+    TCP_Recv(&msg[0], len, 1, OPEN_MSG_TAG);
+    for (int i=0; i<len; i++) {
+      s[i]^=msg[i];
+    }
+    TCP_Recv(&msg[0], len, 2, OPEN_MSG_TAG);
+    for (int i=0; i<len; i++) {
+      s[i]^=msg[i];
+    }
+    // P1 sends result to P2, P3
+    TCP_Send(s, len, 1, OPEN_MSG_TAG);
+    TCP_Send(s, len, 2, OPEN_MSG_TAG);
+    free(msg);
+  }
+  else {
+    fprintf(stderr, "ERROR: Invalid rank %d.\n", rank);
+  }
 }
 
 void reveal_b_array_async(BShare *s, int len)
@@ -510,8 +514,7 @@ void reveal_b_array_async(BShare *s, int len)
 // check if MPI has been initialized
 PRIVATE void check_init(const char *f)
 {
-  // if (!initialized) {
-  //     fprintf(stderr, "ERROR: init() must be called before %s\n", f);
-  //     MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
+  if (!initialized) {
+      fprintf(stderr, "ERROR: init() must be called before %s\n", f);
+  }
 }
