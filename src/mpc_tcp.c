14f33bf6fbdba573e6375bf6bba6736ffe8f0f05
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-11-14 16:01:02 -0500

    Add non-blocking send/recv sig

--- src/mpc_tcp.c
+++ src/mpc_tcp.c
@@ -108,3 +108,105 @@ int TCP_Recv(void *buf, int count, int source, int tag)
     printf("valread: %d ,buf: %s\n", valread, buf);
     return 0;
 }
+
+
+int TCP_Isend(const void *buf, int count, int dest, int tag){
+
+   int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    ssize_t n;
+    const char *p = buf;
+    uint32_t msg_size = htonl((uint32_t)count);
+    send(sock, &msg_size, sizeof(msg_size), 0);
+    while (count > 0)
+    {
+        n = send(sock, buf, count, 0);
+        if (n <= 0)
+            return -1;
+        p += n;
+        count -= n;
+    }
+
+    return 0;
+
+
+
+}
+
+
+int TCP_Irecv(void *buf, int count, int source, int tag){
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    uint32_t msg_size;
+    valread = read(new_socket, &msg_size, sizeof(msg_size));
+    msg_size = ntohl(msg_size);
+    printf("msg_size %d\n", msg_size);
+    valread = read(new_socket, buf, msg_size);
+    printf("valread: %d ,buf: %s\n", valread, buf);
+    return 0;
+
+
+
+}
--- src/mpc_tcp.h
+++ src/mpc_tcp.h
@@ -1,5 +1,14 @@
+#include <sys/poll.h>
 
+struct TCP_Request
+{
 
+    int socket_fd;
+    int count;
+    struct pollfd fds[200];
+    void *buf;
+    int flag; /* send: 0, receive: 1 */
+};
 
 char *get_address(int rank);
 // args: the number of parties, assume we have mapping between rank -> ip:port 
@@ -17,4 +26,11 @@ int TCP_Send(const void *buf, int count, int dest, int tag);
 //  Performs a standard-mode blocking receive.
 int TCP_Recv(void *buf, int count, int source, int tag);
 
-// int MPI_Wait(MPI_Request *request, MPI_Status *status);
+//  Starts a standard-mode, nonblocking send
+int TCP_Isend(const void *buf, int count, int dest, int tag, struct TCP_Request *r);
+
+// Starts a standard-mode, nonblocking receive
+int TCP_Irecv(void *buf, int count, int source, int tag, struct TCP_Request *r);
+
+// Waits for an MPI send or receive to complete.
+int TCP_Wait(struct TCP_Request *r);
