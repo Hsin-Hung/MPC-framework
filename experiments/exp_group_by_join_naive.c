a6a40b7f4e8ee694f12522ded59aa38e14ac9b25
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-12-02 16:03:59 -0500

    Setup exp-group-by-join-naive

--- experiments/exp_group_by_join_naive.c
+++ experiments/exp_group_by_join_naive.c
@@ -55,56 +55,56 @@ int main(int argc, char** argv) {
     rb[i] = (unsigned int) 1;
   }
 
-  // if (rank == 0) { //P1
-  //   // Initialize input data and shares
-  //   Table r1, r2;
-  //   generate_random_table(&r1, ROWS1, COLS);
-  //   generate_random_table(&r2, ROWS2, COLS);
+  if (rank == 0) { //P1
+    // Initialize input data and shares
+    Table r1, r2;
+    generate_random_table(&r1, ROWS1, COLS);
+    generate_random_table(&r2, ROWS2, COLS);
 
-  //   // t1 Bshare tables for P2, P3 (local to P1)
-  //   BShareTable t12 = {-1, 1, ROWS1, 2*COLS, 1};
-  //   allocate_bool_shares_table(&t12);
-  //   BShareTable t13 = {-1, 2, ROWS1, 2*COLS, 1};
-  //   allocate_bool_shares_table(&t13);
+    // t1 Bshare tables for P2, P3 (local to P1)
+    BShareTable t12 = {-1, 1, ROWS1, 2*COLS, 1};
+    allocate_bool_shares_table(&t12);
+    BShareTable t13 = {-1, 2, ROWS1, 2*COLS, 1};
+    allocate_bool_shares_table(&t13);
 
-  //   // t2 Bshare tables for P2, P3 (local to P1)
-  //   BShareTable t22 = {-1, 1, ROWS2, 2*COLS, 2};
-  //   allocate_bool_shares_table(&t22);
-  //   BShareTable t23 = {-1, 2, ROWS2, 2*COLS, 2};
-  //   allocate_bool_shares_table(&t23);
+    // t2 Bshare tables for P2, P3 (local to P1)
+    BShareTable t22 = {-1, 1, ROWS2, 2*COLS, 2};
+    allocate_bool_shares_table(&t22);
+    BShareTable t23 = {-1, 2, ROWS2, 2*COLS, 2};
+    allocate_bool_shares_table(&t23);
     
-  //   init_sharing();
+    init_sharing();
 
-  //   // Generate boolean shares for r1
-  //   generate_bool_share_tables(&r1, &t1, &t12, &t13);
-  //   // Generate boolean shares for r2
-  //   generate_bool_share_tables(&r2, &t2, &t22, &t23);
+    // Generate boolean shares for r1
+    generate_bool_share_tables(&r1, &t1, &t12, &t13);
+    // Generate boolean shares for r2
+    generate_bool_share_tables(&r2, &t2, &t22, &t23);
 
-  //   //Send shares to P2
-  //   MPI_Send(&(t12.contents[0][0]), ROWS1*2*COLS, MPI_LONG_LONG, 1, SHARE_TAG, MPI_COMM_WORLD);
-  //   MPI_Send(&(t22.contents[0][0]), ROWS2*2*COLS, MPI_LONG_LONG, 1, SHARE_TAG, MPI_COMM_WORLD);
+    //Send shares to P2
+    TCP_Send(&(t12.contents[0][0]), ROWS1*2*COLS, 1, sizeof(BShare));
+    TCP_Send(&(t22.contents[0][0]), ROWS2*2*COLS, 1, sizeof(BShare));
 
-  //   //Send shares to P3
-  //   MPI_Send(&(t13.contents[0][0]), ROWS1*2*COLS, MPI_LONG_LONG, 2, SHARE_TAG, MPI_COMM_WORLD);
-  //   MPI_Send(&(t23.contents[0][0]), ROWS2*2*COLS, MPI_LONG_LONG, 2, SHARE_TAG, MPI_COMM_WORLD);
+    //Send shares to P3
+    TCP_Send(&(t13.contents[0][0]), ROWS1*2*COLS, 2, sizeof(BShare));
+    TCP_Send(&(t23.contents[0][0]), ROWS2*2*COLS, 2, sizeof(BShare));
 
-  //   // free temp tables
-  //   free(r1.contents);
-  //   free(t12.contents);
-  //   free(t13.contents);
-  //   free(r2.contents);
-  //   free(t22.contents);
-  //   free(t23.contents);
+    // free temp tables
+    free(r1.contents);
+    free(t12.contents);
+    free(t13.contents);
+    free(r2.contents);
+    free(t22.contents);
+    free(t23.contents);
 
-  // }
-  // else if (rank == 1) { //P2
-  //   MPI_Recv(&(t1.contents[0][0]), ROWS1*2*COLS, MPI_LONG_LONG, 0, SHARE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   MPI_Recv(&(t2.contents[0][0]), ROWS2*2*COLS, MPI_LONG_LONG, 0, SHARE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  // }
-  // else { //P3
-  //   MPI_Recv(&(t1.contents[0][0]), ROWS1*2*COLS, MPI_LONG_LONG, 0, SHARE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   MPI_Recv(&(t2.contents[0][0]), ROWS2*2*COLS, MPI_LONG_LONG, 0, SHARE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  // }
+  }
+  else if (rank == 1) { //P2
+    TCP_Recv(&(t1.contents[0][0]), ROWS1*2*COLS, 0, sizeof(BShare));
+    TCP_Recv(&(t2.contents[0][0]), ROWS2*2*COLS, 0, sizeof(BShare));
+  }
+  else { //P3
+    TCP_Recv(&(t1.contents[0][0]), ROWS1*2*COLS, 0, sizeof(BShare));
+    TCP_Recv(&(t2.contents[0][0]), ROWS2*2*COLS, 0, sizeof(BShare));
+  }
 
   //exchange seeds
   exchange_rsz_seeds(succ, pred);
@@ -154,7 +154,7 @@ int main(int argc, char** argv) {
 
   // apply group-by-count on join output
   // the results are in join_selected_a
-  group_by_count(&res_table, 0, join_selected, join_selected_a, rb, ra);
+  group_by_count(&res_table, 0, 1, join_selected, join_selected_a, rb, ra);
   
   free(join_selected);
   
